8769577b7374cbcd14de536405632b528e163158
tssd-wychen <35886251+tssd-wychen@users.noreply.github.com>
2018-02-24 09:00:18 +0000

    Create py224_2.py

---
+++ pdf_exe_ref/py224_2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+target:extract page within key words, then merge thse pages in one
+"""
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp mport
